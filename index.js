5cc89eae996428e3c9a4aafafcb545418de47461
Cletus Dias <cletus6@gmail.com>
2021-09-22 16:15:42 -0400

    write functions for drag & drop event listeners

--- index.js
+++ index.js
@@ -37,4 +37,50 @@ function createList() {
 			listItems.push(listItem);
 			draggable.appendChild(listItem);
 		});
+
+	addEventListeners();
+}
+
+function dragStart() {
+	dragStartIndex = +this.closest('li').getAttribute('data-index');
+	console.log(dragStartIndex);
+}
+function dragOver(e) {
+	e.preventDefault();
+}
+function dragDrop() {
+	this.classList.remove('over');
+	const dragEndIndex = +this.getAttribute('data-index');
+	console.log(dragEndIndex);
+	swapItems(dragStartIndex, dragEndIndex);
+}
+function dragEnter() {
+	this.classList.add('over');
+}
+function dragLeave() {
+	this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+	const itemOne = listItems[fromIndex].querySelector('.draggable');
+	const itemTwo = listItems[toIndex].querySelector('.draggable');
+	// console.log(itemOne, itemTwo);
+	listItems[fromIndex].appendChild(itemTwo);
+	listItems[toIndex].appendChild(itemOne);
+}
+
+function addEventListeners() {
+	const draggables = document.querySelectorAll('.draggable');
+	const dragListItems = document.querySelectorAll('.draggable-list li');
+
+	draggables.forEach((draggable) => {
+		draggable.addEventListener('dragstart', dragStart);
+	});
+
+	dragListItems.forEach((item) => {
+		item.addEventListener('dragover', dragOver);
+		item.addEventListener('drop', dragDrop);
+		item.addEventListener('dragenter', dragEnter);
+		item.addEventListener('dragleave', dragLeave);
+	});
 }
